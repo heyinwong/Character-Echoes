cffe9b9727cfc839a58b8650e0f7bddeea4f7bd2
Xixian Huang <97789764+heyinwong@users.noreply.github.com>
2025-04-23 13:48:52 +1000

    update option to use streamlit secret

--- core/responder.py
+++ core/responder.py
@@ -1,7 +1,8 @@
 import os
 from openai import OpenAI
+import streamlit as st
 
-api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY", st.secrets.get("OPENAI_API_KEY"))
 client = OpenAI(api_key=api_key)
 
 def get_openai_response(messages, model="gpt-3.5-turbo"):
